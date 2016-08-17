190bead6a3b9feb9e95704f72571268b45e912cf
Fabio Beranizo <fabio.beranizo@gmail.com>
2016-08-17 16:15:50 -0300

    Relatório

--- classifiers/evaluation/evaluation.py
+++ classifiers/evaluation/evaluation.py
@@ -15,7 +15,7 @@ class Evaluation:
     def run(self, classifier, param_grid, X, y):
         """Performs classifier evaluation."""
         print('Evaluating ' + type(classifier).__name__)
-        n_folds = 3
+        n_folds = 10
 
         skf = StratifiedKFold(n_folds=n_folds, shuffle=True)
         for train_index, test_index in skf.split(X, y):
@@ -29,25 +29,24 @@ class Evaluation:
                 print("# Tuning hyper-parameters for %s" % score)
                 print("")
 
-                clf = GridSearchCV(estimator=classifier, param_grid=param_grid, scoring=score, verbose=1, n_jobs=2)
+                clf = GridSearchCV(estimator=classifier, param_grid=param_grid, scoring=score, cv=skf, verbose=1, n_jobs=2)
                 clf.fit(X_train, y_train)
 
+                print("Grid scores on validation set:")
+                print("")
+                results = dict(filter(lambda i:i[0] in ["params", "test_mean_score", "test_std_score", "test_rank_score"], clf.results_.iteritems()))
+                table = PrettyTable()
+                for key, val in results.iteritems():
+                  table.add_column(key, val)
+                print table
+                
                 print("Best parameters set found on validation set:")
                 print("")
                 print(clf.best_params_)
                 print("")
-                print("Grid scores on validation set:")
+
                 print("")
-                results = dict(filter(lambda i:i[0] in ["params", "test_mean_score", "test_std_score"], clf.results_.iteritems()))
-                table = PrettyTable()
-                for key, val in sorted(results.iteritems()):
-                  table.add_column(key, sorted(val))
-                print table
-                #for params, mean_score, scores in clf.results_:
-                #    print("%0.3f (+/-%0.03f) for %r"
-                #          % (mean_score, scores.std() * 2, params))
-                print("")
-                print("Scores on test set:")
+                print("Scores on test set (using best parameters):")
                 print("")
                 y_true, y_pred = y_test, clf.predict(X_test)
                 print classification_report(y_true, y_pred)
